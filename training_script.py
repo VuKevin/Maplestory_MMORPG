d0860fa554257f42bf3cee99de3f8d8749bf3b36
Kevin Vu <VuKevin@icloud.com>
2016-02-14 02:07:48 -0800

    Create training_script.py

---
+++ training_script.py
@@ -0,0 +1,269 @@
+while maple.running:
+	maple.KeyUp(0x10)  # Stop Shifting
+	# maple.MoveX(0) 
+	maple.Sleep(.1) 
+	maple.KeyDown(0x58) 
+	maple.Sleep(9.5) 
+	maple.KeyUp(0x58) 
+	maple.KeyDown(0x11)  # Down CTRL
+	maple.KeyDown(0x10)  # SHIFT
+	maple.MoveX(-230)  # 1
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.KeyDown(0x11) 
+	maple.MoveX(200) 
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.KeyDown(0x11)  # Down CTRL
+	maple.MoveX(-230)  # 2
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.MoveX(0) 
+	maple.Sleep(.3) 
+	maple.KeyDown(0x11) 
+	maple.MoveX(200) 
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.KeyDown(0x11)  # Down CTRL
+	maple.MoveX(-230)  # 3
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.KeyDown(0x11) 
+	maple.MoveX(0) 
+	maple.Sleep(.3) 
+	maple.KeyUp(0x11)  # added here
+	maple.KeyUp(0x10)  # Stop Shifting
+	maple.Sleep(.5) 
+	# SUMMON CYGNUS #2
+	maple.Sleep(.4) 
+	maple.KeyDown(0x32) 
+	maple.Sleep(1.4) 
+	maple.KeyUp(0x32) 
+	maple.KeyDown(0x10)  # SHIFT
+	# CONTINUE
+	maple.KeyDown(0x11)  # Down CTRL # added here
+	maple.MoveX(200) 
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.KeyDown(0x11)  # Down CTRL
+	maple.MoveX(-230)  # 4
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.KeyDown(0x11) 
+	maple.MoveX(200) 
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.KeyDown(0x11)  # Down CTRL
+	maple.MoveX(-230)  # 5
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.KeyDown(0x11) 
+	maple.MoveX(200) 
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.KeyDown(0x11)  # Down CTRL
+	maple.MoveX(-230)  # 6
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.KeyDown(0x11) 
+	maple.MoveX(0) 
+	maple.Sleep(.3) 
+	maple.MoveX(200) 
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.KeyDown(0x11)  # Down CTRL
+	maple.MoveX(-230)  # 7
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.KeyDown(0x11) 
+	maple.MoveX(200) 
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.KeyDown(0x11)  # Down CTRL
+	maple.MoveX(-230)  # 8
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.KeyUp(0x10)  # Stop Shifting
+	maple.Sleep(.5) 
+	# SUMMON CYGNUS #2
+	maple.Sleep(.4) 
+	maple.KeyDown(0x32) 
+	maple.Sleep(1.4) 
+	maple.KeyUp(0x32) 
+	maple.KeyDown(0x10)  # SHIFT
+	# CONTINUE
+	maple.MoveX(0) 
+	maple.Sleep(.3) 
+	maple.KeyDown(0x11) 
+	maple.MoveX(200) 
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.KeyDown(0x11)  # Down CTRL
+	maple.MoveX(-230)  # 9
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.KeyDown(0x11) 
+	maple.MoveX(200) 
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.KeyDown(0x11)  # Down CTRL
+	maple.MoveX(-230)  # 10
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.KeyDown(0x11) 
+	maple.MoveX(200) 
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.KeyDown(0x11)  # Down CTRL
+	maple.MoveX(0) 
+	maple.Sleep(.3) 
+	maple.MoveX(-230)  # 11
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.KeyDown(0x11) 
+	maple.MoveX(200) 
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.KeyDown(0x11)  # Down CTRL
+	maple.MoveX(-230)  # 12
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.KeyDown(0x11) 
+	maple.MoveX(200) 
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.KeyDown(0x11)  # Down CTRL
+	maple.MoveX(-230)  # 13
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.KeyDown(0x11) 
+	maple.MoveX(200) 
+	maple.MoveX(0) 
+	maple.Sleep(.3) 
+	maple.KeyUp(0x11) 
+	maple.KeyUp(0x10)  # Stop Shifting
+	maple.Sleep(.5) 
+	# SUMMON CYGNUS #2
+	maple.Sleep(.4) 
+	maple.KeyDown(0x32) 
+	maple.Sleep(1.4) 
+	maple.KeyUp(0x32) 
+	maple.KeyDown(0x10)  # SHIFT
+	# CONTINUE
+	maple.KeyDown(0x11)  # Down CTRL
+	maple.MoveX(-230)  # 14
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.KeyDown(0x11) 
+	maple.MoveX(200) 
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.KeyDown(0x11)  # Down CTRL
+	maple.MoveX(-230)  # 15
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.KeyDown(0x11) 
+	maple.MoveX(200) 
+	maple.MoveX(0) 
+	maple.Sleep(.3) 
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.KeyDown(0x11)  # Down CTRL
+	maple.MoveX(-230)  # 16
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.KeyDown(0x11) 
+	maple.MoveX(200) 
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.KeyDown(0x11)  # Down CTRL
+	maple.MoveX(-230)  # 17
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.KeyDown(0x11) 
+	maple.MoveX(200) 
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.KeyDown(0x11)  # Down CTRL
+	maple.MoveX(-230)  # 18
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.KeyUp(0x10)  # Stop Shifting
+	# SUMMON CYGNUS #2
+	maple.Sleep(1.4) 
+	maple.KeyDown(0x32) 
+	maple.Sleep(1.4) 
+	maple.KeyUp(0x32) 
+	maple.KeyDown(0x10)  # SHIFT
+	# CONTINUE
+	maple.KeyDown(0x11) 
+	maple.MoveX(200) 
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.KeyDown(0x11)  # Down CTRL
+	maple.MoveX(0) 
+	maple.Sleep(.3) 
+	maple.MoveX(-230)  # 19
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.KeyDown(0x11) 
+	maple.MoveX(200) 
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.KeyDown(0x11)  # Down CTRL
+	maple.MoveX(-230)  # 20
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.KeyDown(0x11) 
+	maple.MoveX(200) 
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.KeyDown(0x11)  # Down CTRL
+	maple.MoveX(-230)  # 21
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.KeyDown(0x11) 
+	maple.MoveX(200) 
+	maple.Sleep(.5) 
+	maple.KeyDown(0x11)  # Down CTRL
+	maple.MoveX(-230)  # 22
+	maple.MoveX(0) 
+	maple.Sleep(.3) 
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.KeyDown(0x11) 
+	maple.MoveX(200) 
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	# maple.KeyDown(0x11)  # Down CTRL
+	# maple.MoveX(-230)  # 23
+	# maple.MoveX(0) 
+	# maple.Sleep(.3) 
+	# maple.KeyUp(0x11) 
+	# maple.Sleep(.5) 
+	maple.KeyDown(0x11) 
+	maple.MoveX(200) 
+	maple.KeyUp(0x11) 
+	maple.Sleep(.5) 
+	maple.KeyUp(0x10)  # Stop Shifting
+	maple.Sleep(.5) 
+	maple.MoveX(0)  # go to the middle
+	#BUFFS
+	maple.Sleep(.9) 
+	maple.KeyPress(0x24)
+	maple.KeyUp(0x32) 
+	maple.KeyDown(0x2E)  # DELETE KEY
+	maple.Sleep(.7) 
+	maple.KeyUp(0x2E) 
+	maple.KeyDown(0x23)  # END KEY
+	maple.Sleep(.7) 
+	maple.KeyUp(0x23) 
+	maple.KeyPress(0x24) 
+	maple.Sleep(.8) 
+	# SUMMON CYGNUS #2
+	maple.Sleep(1.4) 
+	maple.KeyDown(0x32) 
+	maple.KeyPress(0x24) 
+	maple.Sleep(1.4) 
+	maple.KeyUp(0x32) 
+	maple.KeyPress(0x24) 
